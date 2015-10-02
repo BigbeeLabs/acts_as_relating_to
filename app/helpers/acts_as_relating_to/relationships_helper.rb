24bc90a196e01853af39af4d3640cd0fd468ca5c
Joseph <villajosephe@gmail.com>
2015-10-02 12:41:03 -0700

    fixes DE2

--- app/helpers/acts_as_relating_to/relationships_helper.rb
+++ app/helpers/acts_as_relating_to/relationships_helper.rb
@@ -7,5 +7,18 @@ module ActsAsRelatingTo
   		params[:as] ? (owner.relate_to related_thing, as: params[:as]) : (owner.relate_to related_thing)
   		head :ok
   	end
+
+  	def i_relate_to
+			puts "in #{self.class}.#{__method__}, params: #{params}"
+  		owner = controller_constant.find(params["#{controller_name.singularize}_id"])
+  		if params[:as]
+  			things = owner.send "#{params[:thing_type]}_#{__method__}", as: params[:as]
+  		else
+  			things = owner.send "#{params[:thing_type]}_#{__method__}"
+  		end
+  		puts "things: #{things.inspect}"
+  		ret = things.map{|t| [id: t.id, name: t.name, system_name: t.system_name]}
+  		render json: things, status: :ok
+  	end
   end
 end
