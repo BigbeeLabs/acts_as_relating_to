e611ca881b2554150313cf05dcfcafd1ccdfaa04
Joseph <villajosephe@gmail.com>
2015-10-08 13:57:07 -0700

    US208 passed

--- app/models/acts_as_relating_to/relationship.rb
+++ app/models/acts_as_relating_to/relationship.rb
@@ -3,22 +3,20 @@ module ActsAsRelatingTo
 		# ==========================================================================
 		#  Validations
 		# ==========================================================================
-			validates :owner_id,     							presence: true
-			validates :owner_type,   							presence: true
-			validates :in_relation_to_id,   			presence: true
-			validates :in_relation_to_type, 			presence: true
+			validates :owner,     							presence: true
+			validates :in_relation_to,   				presence: true
 			
 		# ==========================================================================
 		#  Associations
 		# ==========================================================================
-			belongs_to  :owner,	polymorphic: true
+			belongs_to  :owner,								polymorphic: true
 			belongs_to  :in_relation_to,    	polymorphic: true
 		
 		# ==========================================================================
 		#  ActsAs Modules
 		# ==========================================================================
-  	acts_as_taggable
-  	acts_as_taggable_on :roles
+      acts_as_taggable
+      acts_as_taggable_on :roles
   	
   end
 end
