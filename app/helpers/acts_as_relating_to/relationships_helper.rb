a34fc83e3bc3753b6b2addf0c42d4292b3d620cd
Joseph <villajosephe@gmail.com>
2016-03-08 14:56:22 -0800

    finally ready to work on message queue

--- app/helpers/acts_as_relating_to/relationships_helper.rb
+++ app/helpers/acts_as_relating_to/relationships_helper.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: acts_as_relating_to_relationships
+#
+#  id                  :integer          not null, primary key
+#  owner_id            :integer
+#  owner_type          :string
+#  in_relation_to_id   :integer
+#  in_relation_to_type :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+
 module ActsAsRelatingTo
   module RelationshipsHelper
   	def relate_to(options={})
