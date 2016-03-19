a7e6577b3ad5ac87e78d0df17631c96166b3c374
Joseph <villajosephe@gmail.com>
2016-03-19 16:31:10 -0700

    cucumber @bootstrap passes

--- lib/acts_as_relating_to/class_methods/optioned.rb
+++ lib/acts_as_relating_to/class_methods/optioned.rb
@@ -2,7 +2,6 @@ module ActsAsRelatingTo
   module ClassMethods
     module Optioned
       def acts_as_relating_to_optioned(spec)
-        puts "in #{self}.#{__method__}, spec: #{spec}"
         options = spec.pop
         class_sym = spec[0]
         acts_as_relating_to_base class_sym, options
